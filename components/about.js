45e5aa10d2f0783c71b29c2ae0cb44bbdcf6bdab
Aarav <43528744+Aarav87@users.noreply.github.com>
2023-04-17 16:35:51 -0400

    added about section

---
+++ components/about.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function About() {
+    return (
+        <div id="about" className="flex flex-col items-center lg:flex-row bg-blue-800 p-10 lg:p-20 xl:px-40 items-left gap-6 z-[30] py-0 lg:pt-8 pb-0 lg:pb-0 xl:pb-0">
+            <div className="flex flex-col items-center lg:items-start w-[80%] text-center lg:text-left mb-4 lg:mb-0 sm:pt-4">
+                <h1 className="text-white font-bold text-3xl md:text-4xl">
+                    Using baseball to {" "}
+                    <span className="text-orange-500">empower</span>
+                    {" "} Canadian youth
+                </h1>
+
+                <p className="mt-4 w-[85%] md:w-[80%] text-lg text-gray-200">
+                    HomeRunsForHope is a charity students vs teachers baseball game
+                    being hosted at St. Francis Xavier SS, Mississauga, on June 1st.
+                    All proceeds will go to the Jays Care Foundation to support the
+                    development of marginalized Canadian youth.
+                </p>
+
+                <div className='my-4'>
+                    <a href="https://www.mlb.com/bluejays/community/jays-care" target="_blank" rel="noreferrer">
+                        <button className='px-6 py-3 border border-blue-400 text-gray-200 hover:bg-blue-400 rounded-lg font-medium'>
+                            Learn more about the Jays Care Foundation
+                        </button>
+                    </a>
+                </div>
+            </div>
+
+            <div className="flex p-0 m-0 w-full md:w-1/2 h-3/4">
+                <Image src="/banner.jpg" alt="Jays Care Foundation in-action" height={500} width={500} className="rounded-lg" />
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
 import Image from "next/image";
+import About from "@/components/about";
 
 export default function Home() {
     return (
@@ -28,6 +29,7 @@ export default function Home() {
                     </div>
                 </div>
             </header>
+
             <div className="-translate-y-40">
                 <div className="h-0">
                     <svg preserveAspectRatio="none" width="100%" height="300px" viewBox="0 0 1440 600" fill="none"
@@ -44,6 +46,31 @@ export default function Home() {
                          xmlns="http://www.w3.org/2000/svg" className="z-[20] relative">
                         <path
                             className="fill-blue-800"
+                            d="M 0,600 C 0,600 0,400 0,400 C 134.85714285714286,415.3571428571429 269.7142857142857,430.7142857142857 378,434 C 486.2857142857143,437.2857142857143 568.0000000000001,428.5 685,417 C 801.9999999999999,405.5 954.2857142857142,391.2857142857143 1086,388 C 1217.7142857142858,384.7142857142857 1328.857142857143,392.3571428571429 1440,400 C 1440,400 1440,600 1440,600 Z"
+                            stroke="none" />
+                    </svg>
+                </div>
+                <div className="h-[130px]" />
+            </div>
+
+            <About />
+
+            <div>
+                <div className="h-0">
+                    <svg preserveAspectRatio="none" width="100%" height="200px" viewBox="0 0 1440 600" fill="none"
+                         xmlns="http://www.w3.org/2000/svg" className="z-[20] relative rotate-180">
+                        <path
+                            className="fill-blue-800"
+                            d="M 0,600 C 0,600 0,400 0,400 C 134.85714285714286,415.3571428571429 269.7142857142857,430.7142857142857 378,434 C 486.2857142857143,437.2857142857143 568.0000000000001,428.5 685,417 C 801.9999999999999,405.5 954.2857142857142,391.2857142857143 1086,388 C 1217.7142857142858,384.7142857142857 1328.857142857143,392.3571428571429 1440,400 C 1440,400 1440,600 1440,600 Z"
+                            stroke="none" />
+                    </svg>
+                </div>
+
+                <div className="h-0">
+                    <svg preserveAspectRatio="none" width="100%" height="300px" viewBox="0 0 1440 600" fill="none"
+                         xmlns="http://www.w3.org/2000/svg" className="z-[20] relative -translate-y-40">
+                        <path
+                            className="fill-blue-500"
                             d="M 0,600 C 0,600 0,400 0,400 C 134.85714285714286,415.3571428571429 269.7142857142857,430.7142857142857 378,434 C 486.2857142857143,437.2857142857143 568.0000000000001,428.5 685,417 C 801.9999999999999,405.5 954.2857142857142,391.2857142857143 1086,388 C 1217.7142857142858,384.7142857142857 1328.857142857143,392.3571428571429 1440,400 C 1440,400 1440,600 1440,600 Z"
                             stroke="none" />
                     </svg>
